815f46d7cc6c7745fc9abd160a94621e0c833a56
Ava Pek <exodrifter@gmail.com>
2017-10-19 06:20:43 -0500

    Add routine cancel and IEnumerator compatability

--- Routine.cs
+++ Routine.cs
@@ -1,11 +1,42 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Exodrifter.Anchor
 {
-	public static class Routine
+	public class Routine
 	{
-		public static void Start(float seconds, Action<float> action)
+		private GameObject go;
+		private Helper helper;
+
+		private Routine(GameObject go, Helper helper)
+		{
+			this.go = go;
+			this.helper = helper;
+		}
+
+		public IEnumerator AsIEnumerator()
+		{
+			while (!Util.IsNull(go) && helper.elapsed < helper.seconds)
+			{
+				yield return null;
+			}
+		}
+
+		public void Cancel()
+		{
+			if (!Util.IsNull(go))
+			{
+				GameObject.Destroy(go);
+			}
+		}
+
+		public static IEnumerator IEnumerator(float seconds, Action<float> action)
+		{
+			return Start(seconds, action).AsIEnumerator();
+		}
+
+		public static Routine Start(float seconds, Action<float> action)
 		{
 			var gameObject = new GameObject("Routine");
 			gameObject.hideFlags = HideFlags.HideAndDontSave;
@@ -13,6 +44,8 @@ namespace Exodrifter.Anchor
 			var helper = gameObject.AddComponent<Helper>();
 			helper.action = action;
 			helper.seconds = seconds;
+
+			return new Routine(gameObject, helper);
 		}
 
 		private class Helper : MonoBehaviour

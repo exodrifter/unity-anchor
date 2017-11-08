c66c2406ca19885d3a9977a0fa076390258ede9c
Ava Pek <exodrifter@gmail.com>
2017-11-08 01:57:13 -0600

    Add DontDestroyOnLoadBehaviour

---
+++ Behaviours/DontDestroyOnLoadBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Exodrifter.Anchor
+{
+	public class DontDestroyOnLoadBehaviour : MonoBehaviour
+	{
+		void Awake()
+		{
+			DontDestroyOnLoad(gameObject);
+		}
+	}
+}

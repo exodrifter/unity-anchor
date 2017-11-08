351dddb025bd835692f44b3c46208955ee9d6240
Ava Pek <exodrifter@gmail.com>
2017-11-08 03:14:39 -0600

    Add Job threading utility

---
+++ Job.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+
+namespace Exodrifter.Anchor
+{
+	public class Job
+	{
+		private readonly Thread thread;
+
+		public bool Done { get; private set; }
+
+		public Job(Action action)
+		{
+			Done = false;
+			thread = new Thread(new ThreadStart(() => {
+				action();
+				Done = true;
+			}));
+			thread.Start();
+		}
+
+		public IEnumerator Wait()
+		{
+			while (!Done)
+			{
+				yield return null;
+			}
+		}
+	}
+}
